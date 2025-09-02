70320d011b39569f13a81d6e7c118a247f440837
Jeff Nichols <JeffN@LYNTRON.LOCAL>
2025-09-02 14:30:18 -0700

    Ported QT35

---
+++ TamperHost/tm-tdd/banners/QT35.dev.header.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         QT35_DEV
+// @namespace    https://github.com/AlphaGeek509/plex-tampermonkey-scripts
+// @version      3.5.239
+// @description  Displays read-only “Attachments: N” in the Quote Wizard action bar.
+// @match        https://*.plex.com/*
+// @match        https://*.on.plex.com/*
+// @require      http://localhost:5000/lt-plex-tm-utils.user.js
+// @require      http://localhost:5000/lt-plex-auth.user.js
+// @grant        GM_registerMenuCommand
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+// @connect      *.plex.com
+// @connect      localhost
+// @run-at       document-idle
+// @noframes
+// ==/UserScript==
