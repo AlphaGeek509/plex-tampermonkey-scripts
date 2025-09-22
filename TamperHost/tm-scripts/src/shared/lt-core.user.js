8dc57921b50146e63ce5ae2db73279251894c961
Jeff Nichols <jnichols@lyntron.com>
2025-09-22 12:37:50 -0700

    Release: v3.7.7 (lockstep) (#2)

--- TamperHost/tm-scripts/src/shared/lt-core.user.js
+++ TamperHost/tm-scripts/src/shared/lt-core.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         lt-core
 // @namespace    lt
-// @version      1.8
+// @version      3.7.7
 // @description  Shared core: auth + http + plex DS + hub (status/toast) + theme bridge + tiny utils
 // @run-at       document-start
 // @grant        none
