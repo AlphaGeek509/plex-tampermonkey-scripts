53c964e4bc03c76ee4a4afd2a3d636b2cf18171b
Jeff Nichols <jnichols@lyntron.com>
2025-09-22 12:43:48 -0700

    release: v3.7.8 (lockstep) (#3)

--- TamperHost/tm-scripts/src/shared/lt-plex-tm-utils.user.js
+++ TamperHost/tm-scripts/src/shared/lt-plex-tm-utils.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LT › Plex TM Utils
 // @namespace    https://github.com/AlphaGeek509/plex-tampermonkey-scripts
-// @version      3.7.7
+// @version      3.7.8
 // @description  Shared utilities
 // @match        https://*.on.plex.com/*
 // @match        https://*.plex.com/*
