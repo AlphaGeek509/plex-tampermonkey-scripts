a09ee06c832b12d8b0a017946d072cb5f1a6baef
Jeff Nichols <jnichols@lyntron.com>
2025-10-03 09:03:16 -0700

    Bumped v4.0.0

--- TamperHost/tm-scripts/src/shared/lt-plex-auth.user.js
+++ TamperHost/tm-scripts/src/shared/lt-plex-auth.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LT › Plex Auth Helper
 // @namespace    https://github.com/AlphaGeek509/plex-tampermonkey-scripts
-// @version      3.9.23
+// @version      4.0.1
 // @description  Shared helper for storing and retrieving Plex API key
 // @match        https://*.on.plex.com/*
 // @match        https://*.plex.com/*
